5b3ba98f9a19acbf7fe0f1d99348dfbbe36fd22f
zumma93 <minemine7593@gmail.com>
2024-04-15 12:53:42 +0900

    config change

---
+++ docs/example.md
@@ -0,0 +1,26 @@
+---
+description: just test.
+sidebar_position: 2
+---
+
+# Docusaurus test
+
+Are you ready to create the documentation site for your open source project?
+
+## bbbb
+
+will show up on the table of contents on the upper right
+
+So that your users will know what this page is all about without scrolling down or even without reading too much.
+
+## ccc
+
+You can configure the TOC heading levels either per-document or in the theme configuration.
+
+The headers are well-spaced so that the hierarchy is clear.
+
+- lists will help you
+- present the key points
+- that you want your users to remember
+  - and you may nest them
+    - multiple times
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://zumma93.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-sandbox/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zumma93', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-sandbox', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
