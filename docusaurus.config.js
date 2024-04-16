91a024ed635424092ea3002f5aade71348d7243f
zumma93 <minemine7593@gmail.com>
2024-04-16 16:03:01 +0900

    Update circle ci config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://zumma93.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-sandbox/',
+  baseUrl: '/docusaurus-sandbox',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
